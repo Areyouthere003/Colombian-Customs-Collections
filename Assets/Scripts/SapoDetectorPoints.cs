c6757bbc7745ffb27fcb733a5559ad7041edc0b6
Gabriel <jgerazo7@misena.edu.co>
2024-09-25 09:54:41 -0500

    update the scene with interaction points in sapo game

--- Assets/Scripts/SapoDetectorPoints.cs
+++ Assets/Scripts/SapoDetectorPoints.cs
@@ -4,30 +4,32 @@ using UnityEngine;
 
 public class SapoDetectorPoints : MonoBehaviour
 {
-    [SerializeField] Transform[] _pointsBase;
-    [SerializeField] Vector3[] _poinstPosition;
+    [SerializeField] int[] valuePoints = { 9, 10, 12, 15, 20, 30, 70, 80, 90, 500};
+    [SerializeField] int valuepointsArray = 0, totalPoints = 0; 
+    [SerializeField] bool valid01 = false;
     int _count = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-       _pointsBase = gameObject.GetComponentsInChildren<Transform>();
 
-        while (_pointsBase.Length > _count)
-        {
-            _poinstPosition[_count] = new Vector3(_poinstPosition[_count].x, _poinstPosition[_count].y, _poinstPosition[_count].z);
-            _count++;
-        }
-    //    for (int i = 0; i < _pointsBase.Length; i++) 
-    //    {
-    //        _poinstPosition[i] = new Vector3(_poinstPosition[i].x, _poinstPosition[i].y, _poinstPosition[i].z);
-    //    }
-        
-    //}
+    }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(valid01)
+        {
+            totalPoints += valuePoints[valuepointsArray];
+            valid01 = false;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Ring"))
+        {
+            valid01 = true;
+        }
     }
 }
